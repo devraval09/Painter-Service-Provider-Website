742d36c9bbac38f99446adcea7ac5878aa2f0d30
Dev Raval <devraval44@gmail.com>
2024-11-20 14:42:23 +0530

    Add files via upload

---
+++ App_Code/ConnectionClass.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Data;
+using System.Data.SqlClient;
+
+/// <summary>
+/// Summary description for ConnectionClass
+/// </summary>
+public class ConnectionClass
+{
+    SqlConnection con;
+    SqlCommand cmd;
+    SqlDataAdapter adp;
+    DataSet ds;
+    DataTable dt;
+
+	public ConnectionClass()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+
+        con = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=|DataDirectory|\painter.mdf;Integrated Security=True");
+        con.Open();
+	}
+
+    public int allQuery(string query)
+    {
+        cmd = new SqlCommand(query, con);
+        return cmd.ExecuteNonQuery();
+    }
+
+    public DataTable allRecord(string query)
+    {
+        cmd = new SqlCommand(query, con);
+        adp = new SqlDataAdapter(cmd);
+        DataSet ds = new DataSet();
+        adp.Fill(ds);
+
+        dt = new DataTable();
+        dt = ds.Tables[0];
+        return dt;
+
+    }
+}
